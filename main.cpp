b62a24c130701a7d39829c0d53027ad726996009
Giulia Mescolini <giulia.mescolini@mail.polimi.it>
2021-03-08 16:31:35 +0000

    Use MyClass in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include"subdirectory/my_class.hpp"
 int main(int argv, char **argv){
+  MyClass m;
   std::cout << "Hello world!" << std::endl;
   std::cout << "This is a new added line" << std::endl;
   return 0;
