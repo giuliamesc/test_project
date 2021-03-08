5804a8473d6c4bb058537dff233dcdfca1249b03
Giulia Mescolini <giulia.mescolini@mail.polimi.it>
2021-03-08 16:29:15 +0000

    Include MyClass from the main application

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include"subdirectory/my_class.hpp"
 int main(int argv, char **argv){
   std::cout << "Hello world!" << std::endl;
   std::cout << "This is a new added line" << std::endl;
